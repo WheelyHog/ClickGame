33b19524d832785baf0fa0c7e4c3af486b9537ae
Sergii <wheelyhog@gmail.com>
2023-05-25 10:25:14 +0200

    add counter

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
+
+
 function start_game() {
   object.classList.toggle('start');
   document.title = 'Score: 0';
   score = 0;
+  paragraph.innerHTML = score
 }
 
 function miss(event) {
   if (event.target.id == "area") {
     score--;
+    paragraph.innerHTML = score
     document.title = `Score: ${score}`;
 
     if (score <= 0) {
-        finish_game();
+      finish_game();
     }
   }
 }
 
 function finish_game() {
-    alert(`You lose. Score ${score}`);
-    start_game();
+  alert(`You lose. Score ${score}`);
+  start_game();
 }
 
 function hit() {
   score++;
+  paragraph.innerHTML = score
   document.title = `Score: ${score}`;
 
   object.classList.remove('start');
   void object.offsetWidth;
   object.classList.add('start');
-  
+
   let random_offset = Math.floor(Math.random() * 340);
   object.style.left = `${random_offset}px`;
 
@@ -39,3 +44,11 @@ let score = 0;
 let object = document.querySelector('#object');
 
 const hit_sound = new Audio('sounds/hit.wav');
+
+// const header = document.getElementById('header')
+const score_p = document.querySelector('score')
+
+const paragraph = document.createElement('p')
+paragraph.innerHTML = score
+const header = document.querySelector('header')
+header.append(paragraph)
